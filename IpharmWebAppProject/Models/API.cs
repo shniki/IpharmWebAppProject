6d9f3a5e1e06bc6651fbbad1e781ef379641f3c6
yuri2010 <yaarisheli@gmail.com>
2021-10-05 14:21:27 +0300

    Finalized twitter post search
    New model (API) will init a twitter posts search for 'ipharm', this model will be passed to home screen to display the data (passage needs to be worked on, fake data for now)

---
+++ IpharmWebAppProject/Models/API.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.ComponentModel.DataAnnotations;
+
+namespace IpharmWebAppProject.Models
+{
+    public class API
+    {
+        [Key]
+        [Required(ErrorMessage = "Posts are required")]
+        [DataType(DataType.Text)]
+        public string posts { get; set; }
+
+        private string getPosts()
+        {
+            var url = "https://api.twitter.com/1.1/search/tweets.json?q=%23ipharm&result_type=recent";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpRequest.Headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAABWVUQEAAAAA3pjPn0RXe93LEdgfdW7f1JdHhbI%3DTtCR0sZtohOeFmmo20FuQeBaXWnp26tWTktL4ZA20ziM81e5VA";
+
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new System.IO.StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                return result;
+            }
+        }
+
+        public API()
+        {
+            posts = getPosts();
+        }
+    }
+}
---
+++ IpharmWebAppProject/Models/Order - Copy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IpharmWebAppProject.Models
+{
+    public enum Status {Cart, Paid, Arrived}
+    public class Order
+    {
+        [Key]
+        [Required]
+        public int OrderId { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public Status Status { get; set; } = Status.Cart;
+
+        public float Price { get; set; } = 0; //$ >=0
+
+        public DateTime OrderDate { get; set; } //today, automatic
+
+        //Products (InOrders)
+        public ICollection<ProductInOrder> Products { get; set; }
+    }
+}
